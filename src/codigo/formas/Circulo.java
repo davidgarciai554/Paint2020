01ee6410a0140f09935026b16a564154d5dba759
David <garciamartindavid@hotmail.es>
2020-01-24 15:21:50 +0100

    Añadido el circulo

--- src/codigo/formas/Circulo.java
+++ src/codigo/formas/Circulo.java
@@ -6,38 +6,15 @@
 package codigo.formas;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
 
 /**
  *
  * @author usuario
  */
-public class Circulo extends Ellipse2D.Double {
-    
-    public Color color = Color.black;
-    public boolean relleno = false;
-    
-    public Circulo(int posX, int posY, int diametro, Color _color, boolean _relleno) {
-        super();
-        x = posX;  //asigno las posX a la propiedad x del constructor de ellips2D.double
-        y = posY;
-        width = diametro;
-        height = diametro;
-        color = _color;
-        relleno = _relleno;
-        
+public class Circulo extends Forma {
+
+    public Circulo(int posX, int posY, int numLados, Color _color, boolean _relleno) {
+        super(posX, posY,306, _color, _relleno);
     }
-    
-    public void dibujate(Graphics2D g2, int posX) {
-        int diametro = Math.abs((int) x - posX);
-        width = diametro;
-        height = diametro;
-        g2.setColor(color);
-        if (relleno) {
-            g2.fill(this);
-        } else {
-            g2.draw(this);
-        }
-    }
+
 }
