bdea4da94a03f63efa661ecf2a3b8cbe758dee72
David <garciamartindavid@hotmail.es>
2020-01-26 18:21:43 +0100

    Rellena el rectangulo pero solo en una direccion

--- src/codigo/formas/Rectangulo.java
+++ src/codigo/formas/Rectangulo.java
@@ -14,22 +14,32 @@ import java.awt.Polygon;
  *
  * @author kahzerx
  */
-public class Rectangulo extends Polygon{
+public class Rectangulo extends Polygon {
+
     public Color color = null;
     public int x = 0;
     public int y = 0;
-    public Rectangulo(int posX, int posY, Color _color) {
+    public boolean relleno = false;
+
+    public Rectangulo(int posX, int posY, Color _color, boolean _relleno) {
         this.x = posX;
         this.y = posY;
 
+        relleno = _relleno;
         color = _color;
     }
-    public void dibujate(Graphics2D g2, int posX, int posY,String grosor) {
+
+    public void dibujate(Graphics2D g2, int posX, int posY, String grosor) {
         g2.setColor(color);
         g2.setStroke(new BasicStroke(Float.parseFloat(grosor)));
         g2.drawLine(x, posY, x, y);
         g2.drawLine(posX, y, x, y);
         g2.drawLine(posX, posY, posX, y);
         g2.drawLine(posX, posY, x, posY);
+        if (relleno) {
+            g2.fillRect(x, y,( posX-x),( posY-y));
+        } else {
+            g2.draw(this);
+        }
     }
 }
